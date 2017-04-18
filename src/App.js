775bc01c612a872ae5c2d54612e8e8198d9b228a
Ricardo Pimienta <rpimienta@itexico.net>
2017-04-18 11:59:02 -0500

    wip: Rendering Elements

--- src/App.js
+++ src/App.js
@@ -2,36 +2,21 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function formatName (user) {
-  return user.firstname + ' ' + user.lastname;
+function tick () {
+  const element = (
+    <div>
+      <h1>Hello!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
 }
-
-const user = {
-  firstname : 'Ricardo',
-  lastname: 'Pimienta'
-};
-
-const element = (
-  <h1>
-    Hello, {formatName(user)}!
-  </h1>
-);
-
 class App extends Component {
   render() {
     return (
-      /*<div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>*/
       element
     );
   }
 }
+setInterval(tick, 2000);
 
 export default App;
