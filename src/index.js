611687a5c68009252cc54665ec51b342928b3eed
Ricardo Pimienta <rpimienta@itexico.net>
2017-04-19 10:14:02 -0500

    State: clock example

--- src/index.js
+++ src/index.js
@@ -1,69 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import logo from './logo.svg';
-import './App.css';
-// import App from './App';
 
-import './index.css';
+class Clock extends React.Component{
+  constructor (props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
 
-function formatDate(date) {
-  return date.toLocaleDateString();
-}
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
 
-const author = {
-  user: {
-    avatarUrl: logo,
-    name: 'Fulanito de Tal'
-  },
-  date: new Date(),
-  text: 'This is a random comment'
-}
-function App () {
-  return (
-    <Comment 
-    user = {author.user}
-    date = {author.date}
-    text = {author.text}
-    />
-  );
-}
-function Comment (props) {
-  console.log(props)
-  return (
-    <div className="Comment">
-      <UserInfo user={props.user}/>
-      <div className="Comment-text">
-        {props.text}
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
       </div>
-      <div className="Comment-date">
-        {formatDate(props.date)}
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
-function UserInfo (props) {
-  return (
-    <div className="UserInfo">
-      <Avatar user={props.user} />
-      <div className="UserInfo-name">
-        {props.name}
-      </div>
-    </div>
-  );
-} 
-
-function Avatar (props) {
-  console.log(props);
-  return (
-    <img 
-      className = "Avatar App-logo"
-      src = {props.user.avatarUrl}
-      alt = {props.user.name}
-    />
-  );
-}
 ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-  );
+  <Clock />,
+  document.getElementById('root')
+);
