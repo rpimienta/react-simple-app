dd85521a85a2b65934819e0852c6062a5289816d
Ricardo Pimienta <rpimienta@itexico.net>
2017-04-19 16:57:53 -0500

    List & Keys : siblings example

--- src/index.js
+++ src/index.js
@@ -1,19 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function NumbersList(props){
-    const numbers = props.array;
-    const list = numbers.map( (item) => 
-      <li key={item}>{item * 2}</li>
+function Blog (props) {
+  const topbar = (
+    <ul>
+      {
+        props.posts.map( (item) => 
+          <li key={item.id}>
+            {item.title}
+          </li>
+        )
+      }
+    </ul>
     );
-    return (
-      <ul>
-        {list}
-      </ul>
-    );
+
+  const footer = props.posts.map( (item) =>
+    <div key={item.id}>
+      <h3>{item.title}</h3>
+      <p>{item.content}</p>
+    </div>
+  );
+
+  return (
+    <div>
+      {topbar}
+      <hr />
+      {footer}
+    </div>
+  );
 }
 
+const posts = [
+  {id: 1, title: 'Hello World', content: 'Welcome to learning React!'},
+  {id: 2, title: 'Installation', content: 'You can install React from npm.'}
+];
 ReactDOM.render(
-  <NumbersList  array={[1,2,3,4,5,6,7]}/>,
+  <Blog posts={posts} />,
   document.getElementById('root')
 );
