9b41540eba59b51ec6692b1f74de5f6241264ebc
Ricardo Pimienta <rpimienta@itexico.net>
2017-04-19 11:42:58 -0500

    Conditional Rendering: Login example

--- src/index.js
+++ src/index.js
@@ -1,31 +1,71 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Toggle extends React.Component{
-  constructor (props) {
-    super(props);
-    this.state = {isToggleOn : true};
-
-    this.handleClick = this.handleClick.bind(this);
+function Greeting(props) {
+  const isLoggedIn = props.isLoggedIn;
+  if (isLoggedIn) {
+    return <UserGreeting />;
   }
+  return <GuestGreeting />;
+}
 
-  handleClick () {
-    this.setState( prevState => ({
-        isToggleOn: !prevState.isToggleOn
-      })
-    );
+function UserGreeting(props) {
+  return <h1>Welcome back!</h1>;
+}
+
+function GuestGreeting(props) {
+  return <h1>Please sign up.</h1>;
+}
+
+function LoginButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Login
+    </button>
+  );
+}
+
+function LogoutButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Logout
+    </button>
+  );
+}
+
+class LoginControl extends React.Component{
+  constructor (props) {
+     super(props);
+     this.handleLoginClick = this.handleLoginClick.bind(this);
+     this.handleLogoutClick = this.handleLogoutClick.bind(this);
+     this.state = {isLoggedIn : true};
+  }
+  handleLoginClick () {
+    this.setState({isLoggedIn: true});
+  }
+  handleLogoutClick () {
+    this.setState({isLoggedIn: false});
   }
   render () {
-    return (
-      <button onClick={this.handleClick}>
-        {this.state.isToggleOn ? 'ON': 'OFF'}
-      </button>
-    )
-  }
+    const isLoggedIn = this.state.isLoggedIn;
 
+    let button = null;
+    if (isLoggedIn) {
+      button = <LogoutButton onClick={this.handleLogoutClick} />;
+    } else {
+      button = <LoginButton onClick={this.handleLoginClick} />;
+    }
+
+    return (
+      <div>
+        <Greeting isLoggedIn={isLoggedIn} />
+        {button}
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(
-  <Toggle />,
+  <LoginControl />,
   document.getElementById('root')
 );
