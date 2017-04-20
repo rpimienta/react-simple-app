3585d16e8fab96ad42c16589934c699d37a3b899
Ricardo Pimienta <rpimienta@itexico.net>
2017-04-20 10:03:08 -0500

    Forms: Controlled Components

--- src/index.js
+++ src/index.js
@@ -1,40 +1,47 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
-function Blog (props) {
-  const topbar = (
-    <ul>
-      {
-        props.posts.map( (item) => 
-          <li key={item.id}>
-            {item.title}
-          </li>
-        )
-      }
-    </ul>
+class NameForm extends Component{
+  constructor (props) {
+    super(props);
+    this.state = { value: 'Please write an essay about your favorite DOM element.'};
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleSubmit (event) {
+    alert("Your name is: " + this.state.value);
+    event.preventDefault();
+  }
+  
+  handleClick (event) {
+    this.setState({value: event.target.value.toUpperCase()});
+  }
+
+  render () {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          Name
+          
+          <textarea type="text" value={this.state.value} onChange={this.handleClick}/>
+          <input type="submit" value="Submit" />
+
+        </form>
+
+        <select>
+          <option value="grapefruit">Grapefruit</option>
+          <option value="lime">Lime</option>
+          <option selected value="coconut">Coconut</option>
+          <option value="mango">Mango</option>
+        </select>
+      </div>
     );
-
-  const footer = props.posts.map( (item) =>
-    <div key={item.id}>
-      <h3>{item.title}</h3>
-      <p>{item.content}</p>
-    </div>
-  );
-
-  return (
-    <div>
-      {topbar}
-      <hr />
-      {footer}
-    </div>
-  );
+  }
 }
 
-const posts = [
-  {id: 1, title: 'Hello World', content: 'Welcome to learning React!'},
-  {id: 2, title: 'Installation', content: 'You can install React from npm.'}
-];
 ReactDOM.render(
-  <Blog posts={posts} />,
+  <NameForm />,
   document.getElementById('root')
 );
