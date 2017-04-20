44912b0f0d8d442bf54b571286ff9d97fd4df09f
Ricardo Pimienta <rpimienta@itexico.net>
2017-04-20 14:23:39 -0500

    Forms: controlling more than 1 input

--- src/index.js
+++ src/index.js
@@ -1,45 +1,53 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
-class FlavorForm extends Component{
-  constructor (props) {
-    super(props);
-    this.state = { value: 'coconut'};
+class Reservation extends Component{
+ constructor (props) {
+   super(props);
+   this.state={
+     isGoing: true,
+     numberOfGuests: 2
+   }
+   this.handleInputChange = this.handleInputChange.bind(this);
+ }
 
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
+ handleInputChange (event) {
+   const target = event.target;
+   const value = target.type === 'checkbox' ? target.checked : target.value;
+   const name = target.name;
 
-  handleSubmit(event) {
-    alert('Your favorite flavor is: ' + this.state.value);
-    event.preventDefault();
-  }
-  render() {
-    return (
-      <div>
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Pick your favorite La Croix flavor:
-          <select value={this.state.value} onChange={this.handleChange}>
-            <option value="grapefruit">Grapefruit</option>
-            <option value="lime">Lime</option>
-            <option value="coconut">Coconut</option>
-            <option value="mango">Mango</option>
-          </select>
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-      
-        <h2>You have selected the <i>{this.state.value}</i> flavour</h2>
-      </div>
+   this.setState({
+     [name]: value
+   });
+ }
+
+render () {
+  return (
+      <form>
+          <label>
+            Is going:
+            <input
+              name="isGoing"
+              type="checkbox"
+              checked={this.state.isGoing}
+              onChange={this.handleInputChange} />
+          </label>
+          <br />
+          <label>
+            Number of guests:
+            <input
+              name="numberOfGuests"
+              type="number"
+              value={this.state.numberOfGuests}
+              onChange={this.handleInputChange} />
+          </label>
+        </form>
     );
   }
+
 }
 
 ReactDOM.render(
-  <FlavorForm />,
+  <Reservation />,
   document.getElementById('root')
 );
